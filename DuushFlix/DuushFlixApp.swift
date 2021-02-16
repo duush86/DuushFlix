91eb0e27b700ec4c552b8e56acd774f7609cbcad
Antonio <j.antonio.oro@gmail.com>
2021-02-16 12:27:23 -0600

    Initial Commit

---
+++ DuushFlix/DuushFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuushFlixApp.swift
+//  DuushFlix
+//
+//  Created by Antonio Orozco on 2/16/21.
+//
+
+import SwiftUI
+
+@main
+struct DuushFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
